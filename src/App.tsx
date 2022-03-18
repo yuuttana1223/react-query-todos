18f702616fe066cd28cb0ebcd0df1e6dcc2f0d39
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2022-03-18 11:54:42 +0900

    🔧 tailwind, eslint, prettierを設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
+import React from "react";
 import { VFC } from "react";
 
 export const App: VFC = () => {
-  return <></>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 };
